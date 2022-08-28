faeaff9ea17fc9e61260fd01364536235457e8ae
Jin <jpar303@gmail.com>
2022-08-28 22:42:05 +0900

    updated: 2022 info

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,31 +1,35 @@
 import React from 'react';
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 import { useTranslations } from 'next-intl';
 const Hero = (props) => {
+  const t = useTranslations('home');
 
-const t = useTranslations('home');
-
-  
+  function handleClick(e) {
+    //    window.location.href = 'https://www.linkedin.com/in/jin-woo-park-87583389/';
+    window.open('https://www.linkedin.com/in/jin-woo-park-87583389/', '_blank');
+  }
 
   return (
-  <>
-    <Section row nopadding>
-      <LeftSection>
-        <SectionTitle main center>
-          {t('welcome')} <br />
-          {t('welcome_1')}
-        </SectionTitle>
-        <SectionText>
-        {t('description')}
-        </SectionText>
-        <Button onClick={props.handleClick}>Learn More</Button>
-      </LeftSection>
-    </Section>
-  </>
-  )
+    <>
+      <Section row nopadding>
+        <LeftSection>
+          <SectionTitle main center>
+            {t('welcome')} <br />
+            {t('welcome_1')}
+          </SectionTitle>
+          <SectionText>{t('description')}</SectionText>
+          <Button onClick={handleClick}>Find out more</Button>
+        </LeftSection>
+      </Section>
+    </>
+  );
 };
 
 export default Hero;
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,50 +1,66 @@
 import { useTranslations } from 'next-intl';
 
-
-
 export const projects = [
   {
-    title: 'Admin Dashboard',
-    description: "developed React.js Admin Dashboard based on CoreUi Template, It provides convenient shortcuts for common management tasks. The dashboard is mostly based on React Hooks and global state managemtn with Redux Tool Kit. ",
-      image: '/images/Admin-Dashboard.png',
-      tags: ['React', 'Type-Script', 'Redux Tool Kit'],
-    source: 'https://github.com/jin-dev/admin_Dashboard',
-   
+    title: 'SNS Project(Clone)',
+    description:
+      'developed React.js Admin Dashboard based on CoreUi Template, It provides convenient shortcuts for common management tasks. The dashboard is mostly based on React Hooks and global state managemtn with Redux Tool Kit. ',
+    image: '/images/SNS_clone_project.png',
+    tags: ['Next.js', 'Type-Script', 'zustand', 'tailwindcss', 'sanity'],
+    source: 'https://next-js-tik-tok-project.vercel.app/',
+
     id: 0,
   },
+
   {
-    title: 'BlockChain Trading Platform',
-    description: "Sustaining a blockChain Trading Platform app based on React Native. applied presentational & container design pattern.",
-    image: '/images/currency-Platform.png',
-    tags: ['React-Native'],
-    source: 'https://google.com',
-    
+    title: 'Admin Dashboard',
+    description:
+      'developed React.js Admin Dashboard based on CoreUi Template, It provides convenient shortcuts for common management tasks. The dashboard is mostly based on React Hooks and global state managemtn with Redux Tool Kit. ',
+    image: '/images/Admin-Dashboard.png',
+    tags: ['React', 'Type-Script', 'Redux Tool Kit'],
+    source: 'https://github.com/jin-dev/admin_Dashboard',
+
     id: 1,
   },
   {
+    title: 'BlockChain Trading Platform',
+    description:
+      'Sustaining a blockChain Trading Platform app based on React Native. applied presentational & container design pattern.',
+    image: '/images/currency-Platform.png',
+    tags: ['React-Native'],
+    source: 'https://google.com',
+
+    id: 2,
+  },
+  {
     title: 'Next.js Core Project',
-    description: "a skeleton Project based on Next.js. SSR(Server-Side Rendering) is applied. ",
+    description:
+      'a skeleton Project based on Next.js. SSR(Server-Side Rendering) is applied. ',
     image: '/images/nextJs-landing.png',
     tags: ['Next.js', 'Styled-component'],
     source: 'https://google.com',
     visit: 'https://github.com/jin-dev/next_js_skeleton.git',
-    id: 2,
+    id: 3,
   },
   {
     title: 'Online-buzz search Platform',
-    description: "This platform provided 8million daily online data such as Facebook, Twitter and Instagram. some of major companies and universites were main clients for their marketing purpose. ",
+    description:
+      'This platform provided 8million daily online data such as Facebook, Twitter and Instagram. some of major companies and universites were main clients for their marketing purpose. ',
     image: '/images/nielsen-Dashboard.png',
     tags: ['Java-Spring', 'Apache Solr', 'Open-Source Analyzer'],
     source: 'https://google.com',
 
-    id: 3,
+    id: 4,
   },
 ];
 
 export const TimeLineData = [
-  { year: 2011, text: 'Finished Bachelor in Computer Science at the University of Auckland', },
-  { year: 2012, text: 'Worked at Qualcomm CDMA Korea', },
-  { year: 2014, text: 'Worked at Nielsen Korea (Full-Stack)', },
-  { year: 2018, text: 'Worked at OkEx Korea (Node.js React Native)', },
-  { year: 2021, text: 'works at U Fintech Hub (React.js) ', },
+  {
+    year: 2011,
+    text: 'Finished Bachelor in Computer Science at the University of Auckland',
+  },
+  { year: 2012, text: 'Worked at Qualcomm CDMA Korea' },
+  { year: 2014, text: 'Worked at Nielsen Korea (Full-Stack)' },
+  { year: 2018, text: 'Worked at OkEx Korea (Node.js React Native)' },
+  { year: 2021, text: 'works at U Fintech Hub (React.js) ' },
 ];
